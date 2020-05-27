ffa9cddb7349ae353fb9862f94f77b1755d0dfcf
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-05-27 14:19:57 -0700

    Update to work with resources file spec

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -7,12 +7,12 @@ export default function Section ({title, tiles}) {
       <h1 className="wgs-section-title">{title}</h1>
       <div className="wgs-section-horizontal-row">
         {tiles.map(tile => (
-          <a href={tile.url} target="_blank" rel="noopener noreferrer" key={tile.title}>
+          <a href={tile.url} target="_blank" rel="noopener noreferrer" key={tile.title} tabindex="0">
             <div className="wgs-tile">
               <img className="wgs-tile-icon" src={tile.icon} alt="Icon" />
               <div className="wgs-tile-body">
                 <h2>{tile.title}</h2>
-                <p>{tile.body}</p>
+                <p>{tile.tagline}</p>
               </div>
             </div>
           </a>
