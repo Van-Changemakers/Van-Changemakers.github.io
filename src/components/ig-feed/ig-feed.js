7440e297b569bc35e43a4fd8014317fa062a37ec
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-05-27 14:18:54 -0700

    Remove IG bio display in preperation for dialogue

--- src/components/ig-feed/ig-feed.js
+++ src/components/ig-feed/ig-feed.js
@@ -3,16 +3,17 @@ import './ig-feed.scss';
 
 import feed from '../../assets/feed.json';
 
+import Button from '../elements/button/button';
+
 export default function IgFeed () {
 
   return (
     <section className="wgs-ig">
-      <h1 className="wgs-section-title">Instagram Feed</h1>
+      <h1 className="wgs-section-title">Instagram Feed <Button>Follow Us</Button></h1>
       <div className="wgs-section-horizontal-row">
         {feed.data.map(post => (
           <div className="wgs-ig-post" key={post.id} tabindex="0">
             <img className="wgs-ig-post-image" src={post.media_url} alt="Post in Instagram Feed" />
-            <p className="wgs-ig-post-bio">{post.caption}</p>
           </div>
         ))}
       </div>
