ad92268b3a2d70b05e401c3a3ef5f24f6b342387
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-06-01 13:54:53 -0700

    Add link to the website to dialogues

--- src/components/dialogue/dialogue.js
+++ src/components/dialogue/dialogue.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './dialogue.scss';
 
+import Button from '../elements/button/button';
+
 export default function Dialogue ({data}) {
 
   document.body.style.overflow = 'hidden';
 
-  const { icon, bio, title, header } = data;
+  const { icon, bio, title, header, url } = data;
   return (
     <>
       <div class="wgs-dialogue-background"></div>
@@ -16,6 +18,7 @@ export default function Dialogue ({data}) {
         </div>
         <main class="wgs-dialogue-main">
           <h1 class="wgs-dialogue-title">{title}</h1>
+          <a href={url} target="_blank" rel="noopener"><Button>Visit Website</Button></a>
           <p class="wgs-dialogue-bio">{bio}</p>
         </main>
       </div>
