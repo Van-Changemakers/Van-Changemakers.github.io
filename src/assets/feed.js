de6980d3b1138987f898674a5f043ae6e573be32
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-06-27 15:21:01 -0700

    Redesign Instagram Feed

---
+++ src/assets/feed.js
@@ -0,0 +1,15 @@
+import ChangemakersLogo from './changemakers_logo.png';
+import Sustainability from './feed/sustainability_2020_06_27.png';
+
+const feed = [
+  {
+    "title": "9 Ways YOU Can Be Sustainable at Home",
+    "tagline": "Easily find and connect with your representatives in all levels of government.",
+    "icon": ChangemakersLogo,
+    "bio": "",
+    "url": "https://www.instagram.com/p/CB9B4gLnF4U/?utm_source=ig_web_copy_link",
+    "header": Sustainability
+  }
+]
+
+export default feed;
--- src/components/ig-feed/ig-feed.js
+++ src/components/ig-feed/ig-feed.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ig-feed.scss';
 
-import feed from '../../assets/feed.json';
+import feed from '../../assets/feed';
 
 import Button from '../elements/button/button';
 
@@ -11,10 +11,14 @@ export default function IgFeed () {
     <section className="wgs-ig">
       <h1 className="wgs-section-title">Instagram Feed <Button>Follow Us</Button></h1>
       <div className="wgs-section-horizontal-row">
-        {feed.data.map(post => (
-          <div className="wgs-ig-post" key={post.id} tabindex="0">
-            <img className="wgs-ig-post-image" src={post.media_url} alt="Post in Instagram Feed" />
-          </div>
+        {feed.map(post => (
+          <a className="wgs-ig-post" key={post.title} tabindex="0" href={post.url} target="_blank" rel="noreferrer noopener">
+            <img className="wgs-ig-post-image" src={post.header} alt="Post in Instagram Feed" />
+            <div class="wgs-ig-post-info">
+              <h1>{post.title}</h1>
+              <p>View on Instagram →</p>
+            </div>
+          </a>
         ))}
       </div>
     </section>
