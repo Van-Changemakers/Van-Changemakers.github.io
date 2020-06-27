f63b5b8465a1a902ac4d8e81f9165d2b27632b8d
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-06-27 16:28:24 -0700

    Change from tabindex to tabIndex

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -13,7 +13,7 @@ export default function Section ({title, tiles}) {
         <h1 className="wgs-section-title">{title}</h1>
         <div className="wgs-section-horizontal-row">
           {tiles.map(tile => (
-            <div className="wgs-tile" key={tile.title} tabindex="0" onClick={() => setOpenDialogue(tile)}>
+            <div className="wgs-tile" key={tile.title} tabIndex="0" onClick={() => setOpenDialogue(tile)}>
               <img className="wgs-tile-icon" src={tile.icon} alt="Icon" />
               <div className="wgs-tile-body">
                 <h2>{tile.title}</h2>
