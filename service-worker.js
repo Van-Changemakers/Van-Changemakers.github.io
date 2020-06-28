47098336b00ff96ccf4e37ec3e30748e2ec46420
jamesbmadden <39145924+jamesbmadden@users.noreply.github.com>
2020-06-28 02:52:42 +0000

    Deploying to master from  @ 421242669137dc41b3c76ff1afb5642910c70683 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d46dfdb0b4a251e83ae20f36692c630f.js"
+  "/precache-manifest.54354e22dc0f9a3c13d6e426f6c8c28c.js"
 );
 
 self.addEventListener('message', (event) => {
