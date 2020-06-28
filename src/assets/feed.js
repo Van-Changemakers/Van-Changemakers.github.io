c9ea4f83430e2741bbae4987a60ae961aa72e6cb
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-06-28 16:49:30 -0700

    triplicate the one post

--- src/assets/feed.js
+++ src/assets/feed.js
@@ -10,6 +10,24 @@ const feed = [
     "url": "https://www.instagram.com/p/CB9B4gLnF4U/?utm_source=ig_web_copy_link",
     "header": Sustainability,
     "platform": "Instagram"
+  },
+  {
+    "title": "9 Ways YOU Can Be Sustainable at Home",
+    "tagline": "Easily find and connect with your representatives in all levels of government.",
+    "icon": ChangemakersLogo,
+    "bio": "",
+    "url": "https://www.instagram.com/p/CB9B4gLnF4U/?utm_source=ig_web_copy_link",
+    "header": Sustainability,
+    "platform": "Instagram"
+  },
+  {
+    "title": "9 Ways YOU Can Be Sustainable at Home",
+    "tagline": "Easily find and connect with your representatives in all levels of government.",
+    "icon": ChangemakersLogo,
+    "bio": "",
+    "url": "https://www.instagram.com/p/CB9B4gLnF4U/?utm_source=ig_web_copy_link",
+    "header": Sustainability,
+    "platform": "Instagram"
   }
 ]
 
