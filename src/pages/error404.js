9fd607e37bea3881e7a8216728d1da1d13b6eb13
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-06-27 18:58:41 -0700

    style 404 page

--- src/pages/error404.js
+++ src/pages/error404.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { useLocation } from 'react-router-dom';
+
 export default function Error404 () {
+
+  let location = useLocation();
+
   return (
-    <>
-      <main className="wgs-main">
-        <h1>404</h1>
-      </main>
-    </>
+    <main className="wgs-main" style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center'}}>
+      <h1 style={{fontSize: '4rem', margin: '0'}}>404</h1>
+      <p>No page for <code>{location.pathname}</code></p>
+    </main>
   );
 }
