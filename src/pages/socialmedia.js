c93d53cf0a782e09aef207fe0662451e6a371517
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-12 21:32:27 -0700

    Add small social media list

--- src/pages/socialmedia.js
+++ src/pages/socialmedia.js
@@ -10,22 +10,22 @@ export default function SocialMedia () {
     <>
       <main className="wgs-main" style={{'padding-top': '64px'}}>
         <h1 className="wgs-section-title">Accounts</h1>
-        <div className="wgs-section-horizontal-row">
-        <a className="wgs-ig-post" key="Instagram" tabIndex="0" href="https://www.instagram.com/we.changemakers/" target="_blank" rel="noreferrer noopener">
-          <img className="wgs-ig-post-image" src={InstagramLogo} alt="" />
-          <div className="wgs-ig-post-info">
-            <h1>Instagram</h1>
-            <p>@we.changemakers →</p>
-          </div>
-        </a>
-        <a className="wgs-ig-post" key="YouTube" tabIndex="0" href="https://www.youtube.com/" target="_blank" rel="noreferrer noopener">
-          <img className="wgs-ig-post-image" src={YoutubeLogo} alt="" />
-          <div className="wgs-ig-post-info">
-            <h1>YouTube</h1>
-            <p>Coming Soon →</p>
-          </div>
-        </a>
-      </div>
+        <div className="wgs-section-horizontal-row small">
+          <a className="wgs-ig-post" key="Instagram" tabIndex="0" href="https://www.instagram.com/we.changemakers/" target="_blank" rel="noreferrer noopener">
+            <img className="wgs-ig-post-image" src={InstagramLogo} alt="" />
+            <div className="wgs-ig-post-info">
+              <h1>Instagram</h1>
+              <p>@we.changemakers →</p>
+            </div>
+          </a>
+          <a className="wgs-ig-post" key="YouTube" tabIndex="0" href="https://www.youtube.com/" target="_blank" rel="noreferrer noopener">
+            <img className="wgs-ig-post-image" src={YoutubeLogo} alt="" />
+            <div className="wgs-ig-post-info">
+              <h1>YouTube</h1>
+              <p>Coming Soon →</p>
+            </div>
+          </a>
+        </div>
         <IgFeed wrap></IgFeed>
       </main>
     </>
