50d7cf5ea473e3c1cd73a275d8e12397f7bf7436
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-12 20:24:33 -0700

    Fix infinite loop component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './footer.scss';
-import Logo from '../../assets/changemakers_logo.png';
+//import Logo from '../../assets/changemakers_logo.png';
 
 export default function Footer () {
   return (
-    <Footer>
-
-    </Footer>
+    <footer>
+      <p>Footer</p>
+    </footer>
   )
 }
