2123799f66cd4bdd9e0963fd7d6417a60dd8a5ee
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-12 20:15:28 -0700

    Add footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.scss';
+import Logo from '../../assets/changemakers_logo.png';
+
+export default function Footer () {
+  return (
+    <Footer>
+
+    </Footer>
+  )
+}
