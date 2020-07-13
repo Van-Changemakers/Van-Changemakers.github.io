f8baca47a66ecfece3f0b361c23ed1eb5189f97d
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-12 21:07:18 -0700

    Add link to social media page

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './footer.scss';
 import Logo from '../../assets/changemakers_logo.png';
 
+import { Link } from 'react-router-dom';
+
 export default function Footer () {
   return (
     <footer className="wgs-footer">
@@ -9,7 +11,8 @@ export default function Footer () {
       <div className="column">
         <h3>Contact</h3>
         <p><a className="link" href="mailto:">Email Us</a></p>
-        <p>Report site issue on <a className="link" href="https://github.com/We-Changemakers/We-Changemakers.github.io/issues">GitHub</a></p>
+        <p><Link className="link" to="/socialmedia">Social Media Accounts</Link></p>
+        <p>Report site issue on <a className="link" href="https://github.com/We-Changemakers/We-Changemakers.github.io/issues" target="_blank" rel="noreferrer noopener">GitHub</a></p>
       </div>
     </footer>
   )
