5f9854909bf5c9893b70b1ceae2c49c4f8fa9de3
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-12 20:50:25 -0700

    Design Footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './footer.scss';
-//import Logo from '../../assets/changemakers_logo.png';
+import Logo from '../../assets/changemakers_logo.png';
 
 export default function Footer () {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="wgs-footer">
+      <img alt="Changemakers Logo" src={Logo} />
+      <div className="column">
+        <h3>Contact</h3>
+        <p><a className="link" href="mailto:">Email Us</a></p>
+        <p>Report site issue on <a className="link" href="https://github.com/We-Changemakers/We-Changemakers.github.io/issues">GitHub</a></p>
+      </div>
     </footer>
   )
 }
