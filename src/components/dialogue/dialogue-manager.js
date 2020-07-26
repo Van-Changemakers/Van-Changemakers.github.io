d6f8b61b8b63f5e6ba5861034dd51bc40461fa5c
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-26 15:30:03 -0700

    Add floating action buttons to dialogue

--- src/components/dialogue/dialogue-manager.js
+++ src/components/dialogue/dialogue-manager.js
@@ -60,6 +60,18 @@ export default function DialogueManager ({ data }) {
         <Dialogue data={data[index]} key={index} index={index} />
         {(!isLastEntry) ? <Dialogue data={data[index + 1]} key={index + 1} index={index + 1} /> : <></>}
       </div>
+      <div className="wgs-dialogue-fab-bar">
+        <button class="wgs-fab wgs-fab-small"><span class="wgs-fab-inner">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/></svg>
+        </span></button>
+        <button class="wgs-fab wgs-fab-big"><span class="wgs-fab-inner">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
+          <span class="wgs-fab-text">Close</span>
+        </span></button>
+        <button class="wgs-fab wgs-fab-small"><span class="wgs-fab-inner">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg>
+        </span></button>
+      </div>
     </div>
   )
 
