5963eea9e9c3b16f7968f5dd5c24850e01256b99
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-25 23:58:56 -0700

    Fix instagram link

--- src/pages/socialmedia.js
+++ src/pages/socialmedia.js
@@ -11,11 +11,11 @@ export default function SocialMedia () {
       <main className="wgs-main" style={{'padding-top': '64px'}}>
         <h1 className="wgs-section-title">Accounts</h1>
         <div className="wgs-section-horizontal-row small">
-          <a className="wgs-ig-post" key="Instagram" tabIndex="0" href="https://www.instagram.com/we.changemakers/" target="_blank" rel="noreferrer noopener">
+          <a className="wgs-ig-post" key="Instagram" tabIndex="0" href="https://www.instagram.com/van.changemakers/" target="_blank" rel="noreferrer noopener">
             <img className="wgs-ig-post-image" src={InstagramLogo} alt="" />
             <div className="wgs-ig-post-info">
               <h1>Instagram</h1>
-              <p>@we.changemakers →</p>
+              <p>@van.changemakers →</p>
             </div>
           </a>
           <a className="wgs-ig-post" key="YouTube" tabIndex="0" href="https://www.youtube.com/" target="_blank" rel="noreferrer noopener">
