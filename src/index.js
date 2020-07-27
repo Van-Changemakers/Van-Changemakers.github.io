bb21d4e9d81c94ec0febfd9a4c37bfbdcfcc609c
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-07-26 19:15:41 -0700

    add mwc ripple

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import '@material/mwc-ripple';
+
 import 'focus-visible';
 
 import WeSite from './components/wesite';
