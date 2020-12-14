8a6bf018bda148bedc61d3ddd6e04277e1ac603e
James Madden <jamesbmadden@gmail.com>
2020-12-13 19:18:51 -0800

    Refine UI

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -43,7 +43,7 @@ export default function Home () {
 
   return (
     <>
-      <main className="wgs-main" style={{'padding-top': '64px', 'min-height': '100vh'}}>
+      <main className="wgs-main shop" style={{'padding-top': '64px', 'min-height': '100vh'}}>
         <h1 className="wgs-section-title" style={{'text-align': 'center'}}><img src={ChangemakersLogo} alt="Changemakers" style={{'height': '1em', 'margin-right': '1rem'}} />Shop</h1>
         <h1 style={{'text-align': 'center', 'font-style': 'italic', 'font-weight': '400'}}>products for a good cause</h1>
         <h1 className="wgs-section-title" style={{'text-align': 'center'}}>Holiday Cards</h1>
@@ -70,29 +70,29 @@ export default function Home () {
                 </a>
               )
             })}
-
-            <button class="wgs-fab wgs-fab-big wgs-shop-cart-button" onClick={() => {
-              setMobileCartOpen(!mobileCartOpen);
-            }}><span class="wgs-fab-inner">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48px" height="48px"><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg>
-              <span class="wgs-fab-text">{mobileCartOpen ? 'Close' : 'Cart (' + (cart[0] + cart[1] + cart[2] + cart[3] + cart[4]) + ')'}</span>
-            </span><mwc-ripple primary></mwc-ripple></button>
           </div>
           <div className={"wgs-shop-cart " + (mobileCartOpen ? 'open' : 'closed')}>
-            <h2>Cart</h2>
+            <h2><button className="wgs-fab wgs-fab-small wgs-shop-cart-button" onClick={() => {
+              setMobileCartOpen(!mobileCartOpen);
+            }}><span className="wgs-fab-inner">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48px" height="48px"><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg>
+            </span><mwc-ripple primary></mwc-ripple></button><span className="title">Cart</span></h2>
             {cart.map((count, index) => {
               if (count > 0) {
                 return (
-                  <div class="wgs-shop-cart-item">
+                  <div className="wgs-shop-cart-item">
                     <img src={Cards[index].image} alt="Cart Item" />
                     <p>x {count}</p>
-                    <button class="wgs-fab wgs-fab-small" onClick={() => {
+                    <button className="wgs-fab wgs-fab-small" onClick={event => {
                       let newCart = cart;
                       newCart[index]--;
                       console.log(newCart);
                       setCart(newCart);
-                      forceRerender(rerender + 1);
-                    }}><span class="wgs-fab-inner">
+                      event.target.parentElement.parentElement.classList.add('exit');
+                      setTimeout(() => {
+                        forceRerender(rerender + 1);
+                      }, 400);
+                    }}><span className="wgs-fab-inner">
                       -
                     </span><mwc-ripple></mwc-ripple></button>
                   </div>
