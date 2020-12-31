e81af7720e179baaa324405600ef3e61db75a7c0
James Madden <jamesbmadden@gmail.com>
2020-12-30 19:58:58 -0800

    Add helmet for head stuff

--- src/components/wesite.js
+++ src/components/wesite.js
@@ -8,10 +8,21 @@ import 'focus-visible';
 import ChangemakersLogoWhite from '../assets/changemakers_logo_white.png';
 
 import { Link } from 'gatsby';
+import { Helmet } from "react-helmet"
 
 export default function WeSite ({ children }) {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Van Changemakers</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Vancouver Changemakers Site"
+        />
+        
+      </Helmet>
       <div className="wgs-header-links">
         <Link to="/"><img className="wgs-home-link" src={ChangemakersLogoWhite} alt="Changemakers Logo" /></Link>
         <Link className="textlink" to="/shop">Shop</Link>
