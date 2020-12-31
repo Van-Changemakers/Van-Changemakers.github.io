b042e4e15dfb56c58280fb39fc80ca712e9b50fd
James Madden <jamesbmadden@gmail.com>
2020-12-30 19:51:12 -0800

    Create manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        short_name: "Changemakers",
+        name: "Vancouver Changemakers",
+        start_url: `/`,
+        icon: `src/assets/app_logo_1000.png`,
+        icon_options: {
+          purpose: `any maskable`,
+        },
+        background_color: `#13c6a2`,
+        theme_color: `#13c6a2`,
+        display: `standalone`,
+      },
+    },
+  ]
 }
