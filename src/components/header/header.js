8c3cb6054e4a1660ddec71defbffd7d543e51760
James Madden <jamesbmadden@gmail.com>
2021-01-02 14:51:53 -0800

    Add class to toggle new margin on title if highlights are visible

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ import Calendar from './calendar/calendar';
 export default function Header ({ title = "Changemakers", subtitle = "VANCOUVER", image = Skyline, showCalendar = true}) {
   return (
     <>
-      <header className="wgs-header" style={{backgroundImage: `linear-gradient(90deg, rgba(24, 174, 232, 0.75), rgba(19, 198, 163, 0.75)), url(${image})`}}>
+      <header className={`wgs-header ${showCalendar ? 'calendar-visible' : ''}`} style={{backgroundImage: `linear-gradient(90deg, rgba(24, 174, 232, 0.75), rgba(19, 198, 163, 0.75)), url(${image})`}}>
         <div className="wgs-header-title-box">
           <h3>{subtitle}</h3>
           <h1>{title}</h1>
